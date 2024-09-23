1a98c0893acb4581333f67f098dc2326222955f7
SHZTD <tdrag@elpuig.xeill.net>
2024-09-23 16:30:38 +0200

    args

--- src/Pre/Main.java
+++ src/Pre/Main.java
@@ -2,6 +2,6 @@ package Pre;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Empezamos con args...");
+        System.out.println("Empezamos con args...\nArg1: " + args[0]);
     }
 }
