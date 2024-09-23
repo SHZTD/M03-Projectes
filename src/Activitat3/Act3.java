7ee505d04c1ec0de688cb9a637b3d895417df61c
SHZTD <tdrag@elpuig.xeill.net>
2024-09-23 18:27:29 +0200

    totes les activitats acabades

--- src/Activitat3/Act3.java
+++ src/Activitat3/Act3.java
@@ -9,10 +9,11 @@ public class Act3 {
             una cadena de text, especificant formats adequats amb 3 decimals amb i sense signe, amb separador de milers i zeros a l'esquerra.
          */
         int v1 = 69420;
-        float f1 = 69.420f;
-        String st = "sample";
+        float f1 = 420.6905f;
+        String st = "text string testing";
 
-        //String format = String.format();
+        String formatString = String.format("INTEGER -> %d, FLOAT -> %.4f, STRING %s", v1, f1, st);
+        System.out.println(formatString);
 
         /*
             Escriu un programa que utilitzi diverses combinacions de flags i especificadors amb printf per mostrar les variables amb
---
+++ src/Activitat4/Act4.java
@@ -0,0 +1,9 @@
+package Activitat4;
+
+public class Act4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Arguments utilitzats:" + args[i] + ", argnum " + i);
+        }
+    }
+}
